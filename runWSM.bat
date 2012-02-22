e01d36948e1f42ef47c383988dce8d647fdb69f3
samita sarkar <ssarkar@cap.org>
2012-02-22 11:50:28 -0600

    upload

---
+++ runWSM.bat
@@ -0,0 +1,2 @@
+SET CLASSPATH=%CLASSPATH%;C:\Oracle\fmw\modules\oracle.oes.sm_11.1.1\oes-ws-client.jar
+java -Doracle.security.jps.config=C:\Oracle\fmw\oes_sm_instances\labsystemsecurityWSM\config\wsclient\jps-config.xml HelloOESworld
