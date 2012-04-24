8e5e5fda27101c41bb45bc3d9f081a77a4ca589c
samita sarkar <ssarkar@cap.org>
2012-04-24 14:52:05 -0500

    changes for laptop

--- runJavaSM.bat
+++ runJavaSM.bat
@@ -1,4 +1,4 @@
 SET CLASSPATH=%CLASSPATH%;C:\Oracle\product\11.1.1\as_1\modules\oracle.oes.sm_11.1.1\oes-client.jar
 
 
-java -Doracle.security.jps.config=C:\Oracle\product\11.1.1\as_1\oes_sm_instances\labsystemsecurity\config\jps-config.xml HelloOESworld
+java -Doracle.security.jps.config=C:\Oracle\product\11.1.1\as_1\oes_sm_instances\LTPJavaSM\config\jps-config.xml HelloOESworld
