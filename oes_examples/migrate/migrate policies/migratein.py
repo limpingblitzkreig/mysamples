4ca7c61f1268c78380a58d1f073d26bf5f278efe
samita sarkar <ssarkar@cap.org>
2013-02-12 13:17:06 -0600

    new scripts

---
+++ oes_examples/migrate/migrate policies/migratein.py
@@ -0,0 +1,1 @@
+migrateSecurityStore(type="appPolicies", src="sourceContext", dst="destinationContext", configFile="jps-config-migratein.xml", srcApp="TESTMIGRATE", dstApp="TESTMIGRATE")
---
+++ oes_examples/migrate/migrate policies/migrateout.py
@@ -0,0 +1,1 @@
+migrateSecurityStore(type="appPolicies", src="sourceContext", dst="destinationContext", configFile="jps-config-migrateout.xml", srcApp="CAP", dstApp="CAP")
